47793ca91a636469d98d5d876a895432c2a23d2a
Leonardo Lazzarin <lnrdlazzarin@gmail.com>
2026-04-14 11:16:12 -0300

    V6

--- src/services/livrosServices.js
+++ src/services/livrosServices.js
@@ -1,31 +1,20 @@
-const acervo =[
-    {
-        id:1,
-        titulo: "A Biografia de Lucas Pimentel - O Honrado",
-        autor: "Lucas Pimentel Bento",
-        disponivel: false
-    },
-    {
-        id:2,
-        titulo: "Elden Ring Custa 300 Reais com a Dlc Fica 500, Japoneses Safados Querem meu Dinheiro...",
-        autor: "G.R.R Martin & Hidetaka Myiazaki",
-        disponivel: true
-    },
-    {
-        id:3,
-        titulo: "Carros 10 - Carros VS A Ditadura",
-        autor: "J.R.R Tolkien, G.R.R Martin, Quentin Tarantino, Celso Ramos, Crescida Carwell, Padre Marcelo de Rossi, Machado de Assis, Fernando Pessoa, Julio Verne, Akira Toriyama",
-        disponivel: true
-    }
-];
+const pool = require('../database/connection')
 
 const buscarLivros = async () => {
-    return acervo;
+    try {
+        const result = await pool.query('SELECT * FROM livros ORDER BY id')
+        return result.rows
+    } catch(error){
+        console.log('erro', error)
+        throw error
+    }
 };
 
 const buscarLivrosPorID = async (id) => {
-    const livro = acervo.find(item => item.id === Number(id));
-    return livro || null;
+    const result = await pool.query('SELECT * FROM livros WHERE id = $1', [
+        id
+    ])
+    return result.rows(0)
 }
 
 const criarLivro = async => ({titulo, autor}) =>{
